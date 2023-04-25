193e41790d968fc9409a16fb6901b2f44a281432
wispwill <laurajos@epix.net>
2023-04-24 21:50:38 -0400

    Added background, clocktower, and basic points

--- Game.js
+++ Game.js
@@ -1,7 +1,21 @@
+
+var controls;
+var points = 0;
+
+var croissant;
+var walker;
+
+var velocities = new Map([
+    ["walker", 160]
+])
+
+var gamewidth = 1200;
+var gameheight = 800;
+
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: gamewidth,
+    height: gameheight,
     physics: {
         default: 'arcade',
         arcade: {
@@ -12,53 +26,79 @@ var config = {
         preload: preload,
         create: create,
         update: update
-    }
-    //backgroundColor: '#2dab2d'
+    },
+    backgroundColor: '#45b3e0'
 };
 
-var gamewidth = 800;
-var gameheight = (600 * 2);
-
 var game = new Phaser.Game(config);
-controls;
 
 function preload ()
 {
+    this.load.image('background', 'assets/background.png');
     this.load.image('croissant', 'assets/cat.png');
     this.load.image('walker', 'assets/walker1.png');
-    this.load.image('sky', 'assets/sky.png');
+    this.load.image('clocktower', 'assets/clocktower1.png');
+    this.load.spritesheet('clockface', 
+    'assets/clockface_map.png',
+    { frameWidth: 123, frameHeight: 119 }
+);
 }
 
 function update (time, delta)
 {
     this.controls.update(delta);
+
+    if (cursors.left.isDown)
+    {
+        forwardTime();
+    }
+    else if (cursors.right.isDown)
+    {
+        reverseTime();
+    }
+}
+
+function reverseTime ()
+{
+    walker.setVelocityX(-(velocities.get("walker")));
+    clockface.anims.play('face_rotation', true);
+}
+
+function forwardTime ()
+{
+    walker.setVelocityX(velocities.get("walker"));
+    clockface.anims.play('face_rotation', true);
+}
+
+function chowdown (croissant, clockface)
+{
+    croissant.disableBody(true, true);
+    points += 1;
+    scoreText.setText('Pastries Consumed: ' + points)
+    this.tweens.add({
+        targets: clockface, //your image that must spin
+        rotation: 2 * Math.PI, //rotation value must be radian
+        duration: 2000 //duration is in milliseconds
+    });
 }
 
 function create ()
 {
-    this.cameras.main.setBounds(0, 0, gamewidth, gameheight);
-    this.cameras.main.scrollY = gameheight/2;
+    this.add.image(gamewidth/2, gameheight/2,'background');
+    scoreText = this.add.text(140, 65, 'Pastries Consumed: ' + points, { fontSize: '20px', fill: '#9c640c' });
+    clocktower = this.add.image(gamewidth/2, (gameheight/2) + 100, 'clocktower');
+    clockface = this.physics.add.sprite(gamewidth/2 + 2, (gameheight/2) - 6, 'clockface');
+    clockface.body.stop();
+    clockface.body.allowGravity = false;
+    this.anims.create({
+        key: 'face_rotation',
+        frames: this.anims.generateFrameNumbers('clockface', { start: 0, end: 2 }),
+        frameRate: 1,
+        repeat: -1
+    });
+    
 
-    this.physics.world.setBounds(0, 0, gamewidth, gameheight);
-    this.add.image(0, 0, 'sky').setOrigin(0, 0);
-    this.add.image(0, 600, 'sky').setOrigin(0, 0);
-    this.add.image(0, 1200, 'sky').setOrigin(0, 0);
-    this.add.image(0, 1800, 'sky').setOrigin(0, 0);
-
-    var croissant = this.physics.add.image(50, 50, 'croissant');
-    var walker = this.physics.add.image(gamewidth/2, gameheight, 'walker').setScale(0.3);
-
-    croissant.setVelocity(200, 200);
-    croissant.setBounce(1, 1);
-    croissant.setCollideWorldBounds(true);
-
-    walker.setVelocity(200, 0);
-    walker.setBounce(1, 0);
-    walker.setCollideWorldBounds(true);
-
-    this.physics.add.collider(croissant, walker);
-
-    const cursors = this.input.keyboard.createCursorKeys();
+    cursors = this.input.keyboard.createCursorKeys();
 
     const controlConfig = {
         camera: this.cameras.main,
@@ -70,4 +110,22 @@ function create ()
     };
 
     this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
+    this.cameras.main.setBounds(0, 0, gamewidth, gameheight);
+    this.cameras.main.scrollY = gameheight/2;
+
+    this.physics.world.setBounds(0, 0, gamewidth, gameheight);
+
+    croissant = this.physics.add.image(0, 0, 'croissant');
+    walker = this.physics.add.image(gamewidth/2, gameheight, 'walker').setScale(0.5);
+
+    croissant.setVelocity(200, 200);
+    croissant.setBounce(1, 1);
+    croissant.setCollideWorldBounds(true);
+
+    walker.setVelocity(200, 0);
+    walker.setBounce(1, 0);
+    walker.setCollideWorldBounds(true);
+
+    this.physics.add.collider(croissant, walker);
+    this.physics.add.overlap(croissant, clockface, chowdown, null, this);
 }
